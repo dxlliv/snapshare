b6e40918b7bfb964a614c58c62298e6d237e0c09
dxlliv <dvilla8.dev@gmail.com>
2023-05-24 11:56:48 +0200

    Connect to the ws server defined in config.js

---
+++ client/config.js
@@ -0,0 +1,3 @@
+window.snapshareConfig = {
+  SERVER: 'ws://localhost:8080/server'
+}
--- client/scripts/network.js
+++ client/scripts/network.js
@@ -59,8 +59,8 @@ class ServerConnection {
         // hack to detect if deployment or development environment
         const protocol = location.protocol.startsWith('https') ? 'wss' : 'ws';
         const webrtc = window.isRtcSupported ? '/webrtc' : '/fallback';
-        const url = protocol + '://' + location.host + location.pathname + 'server' + webrtc;
-        return url;
+        // const url = protocol + '://' + location.host + location.pathname + 'server' + webrtc;
+        return window.snapshareConfig.SERVER + webrtc;
     }
 
     _disconnect() {
